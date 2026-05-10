8f1e880a3c23c18034629cfa6e082acecbafc4f1
Saiful Alom <hello@saifulalom.com>
2026-05-10 08:14:08 +0600

    fix: linting

--- src/ui/post-card.tsx
+++ src/ui/post-card.tsx
@@ -3,8 +3,6 @@ import UserProfile from "./user-profile";
 import profilePic from '../assets/images/porfile.jpg'
 import postImage from '../assets/images/post-image.jpg'
 
-const image = ""
-
 export default function PostCard() {
     return (
         <div className="w-full border-b p-4 space-y-4 border-[#DDD2C3]">
